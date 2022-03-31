31fbd9003c483516a761f956aeb68298c7798721
ahmed-razic <ahmed.razic@gmail.com>
2022-03-31 20:16:46 +0200

    finished html

--- app.js
+++ app.js
@@ -3,17 +3,3 @@ const english = ['house', 'cat', 'car'];
 
 localStorage.setItem('bosnian', JSON.stringify(bosnian));
 localStorage.setItem('english', JSON.stringify(english));
-
-const inputForm = document.querySelector('#input-form');
-const inputBos = document.querySelector('#input-bos');
-const inputEng = document.querySelector('#input-eng');
-const inputFilter = document.querySelector('#input-filter');
-const wordsList = document.querySelector('#words-list');
-const clearWords = document.querySelector('#clear-words');
-
-console.log(inputForm);
-console.log(inputBos);
-console.log(inputEng);
-console.log(inputFilter);
-console.log(wordsList);
-console.log(clearWords);
