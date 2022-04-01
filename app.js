f44cc241a4ef04c8af2bb87367c3930f83be3bb4
ahmed-razic <ahmed.razic@gmail.com>
2022-04-01 11:18:11 +0200

    add and display words

--- app.js
+++ app.js
@@ -33,25 +33,80 @@ function getAllWords() {
     deleteIcon.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(deleteIcon);
 
-    const span1 = document.createElement('span');
-    span1.className = 'col s6';
-    span1.appendChild(document.createTextNode(words[i]));
-    li.appendChild(span1);
+    const engWord = document.createElement('span');
+    engWord.className = 'col s6';
+    engWord.appendChild(document.createTextNode(words[i]));
+    li.appendChild(engWord);
 
-    const span2 = document.createElement('span');
-    span2.className = 'col s6';
-    span2.appendChild(document.createTextNode(words[i + 1]));
-    li.appendChild(span2);
+    const bosWord = document.createElement('span');
+    bosWord.className = 'col s6';
+    bosWord.appendChild(document.createTextNode(words[i + 1]));
+    li.appendChild(bosWord);
 
     wordsList.appendChild(li);
   }
 }
+
+function addWords(e) {
+  e.preventDefault();
+
+  if (engInput.value === '' || bosInput.value === '') {
+    alert('Pllease enter both words');
+  }
+  const li = document.createElement('li');
+  li.className = 'collection-item blue-grey darken-1 row white-text';
+
+  const deleteIcon = document.createElement('a');
+  deleteIcon.className = 'delete-item secondary-content';
+  deleteIcon.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(deleteIcon);
+
+  const engWord = document.createElement('span');
+  engWord.className = 'col s6';
+  engWord.appendChild(document.createTextNode(engInput.value));
+  li.appendChild(engWord);
+
+  const bosWord = document.createElement('span');
+  bosWord.className = 'col s6';
+  bosWord.appendChild(document.createTextNode(bosInput.value));
+  li.appendChild(bosWord);
+
+  wordsList.appendChild(li);
+
+  storeWords(engInput.value, bosInput.value);
+
+  engInput.value = '';
+  bosInput.value = '';
+}
+
+function storeWords(engWord, bosWord) {
+  let words;
+
+  if (localStorage.getItem('words') === null) {
+    words = [];
+  } else {
+    words = JSON.parse(localStorage.getItem('words'));
+  }
+
+  words.push(engWord);
+  words.push(bosWord);
+
+  localStorage.setItem('words', JSON.stringify(words));
+}
+
+function removeWords() {}
+
+function filterWords() {}
+
+function clearAllWords() {
+  while (wordsList.firstChild) {
+    wordsList.removeChild(wordsList.firstChild);
+  }
+
+  localStorage.clear();
+}
+
 /* 
 
 localStorage.setItem('bosnian', JSON.stringify(bosnian));
 localStorage.setItem('english', JSON.stringify(english)); */
-
-<li class='collection-item blue-grey darken-1 row white-text'>
-  <span class='col s6'>ddd</span>
-  <span class='col s6'>ddd</span>
-</li>;
